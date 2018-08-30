c9953d7c8c9aab7f75f2153884230e10086bbafe
Rolexboy8008 <33845401+Rolexboy8008@users.noreply.github.com>
2018-08-30 17:08:32 +0200

    alle files hoop ik
    denk ik

---
+++ j/PWS LEGIT/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  PWS LEGIT
+//
+//  Created by Koen Frankena on 30-08-18.
+//  Copyright © 2018 Koen Frankena. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
